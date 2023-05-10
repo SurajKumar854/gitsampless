6a2b149746717187ef1ea02b2cb5620dc30a6e93
Surajkumar854 <ersuraj854@gmail.com>
2023-05-10 18:24:06 +0530

    ihuiuh

---
+++ app/src/main/java/com/suraj854/myapplicationgdgdgdgd/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.suraj854.myapplicationgdgdgdgd
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.suraj854.myapplicationgdgdgdgd.ui.theme.MyApplicationgdgdgdgdTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyApplicationgdgdgdgdTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                    dgfdg
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MyApplicationgdgdgdgdTheme {
+        Greeting("Android")
+    }
+}
